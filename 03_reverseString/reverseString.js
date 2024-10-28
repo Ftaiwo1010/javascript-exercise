c1584cdda1f6ac08b2ee4e5fca236c57d1ec6f42
Oluwafemi <ftaiwo1010@gmail.com.noreply.github.com>
2024-10-28 10:36:02 +0100

    commit to github

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   let splitString = string.split('');
+   let reverseText = splitString.reverse();
+   let joinString = reverseText.join('');
+   return joinString;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+   let isYearDivisibleByFour = year % 4 === 0;
+   let century = year % 100 === 0;
+   let isYearDivisibleByFourHundred = year % 400 === 0;
 
+   if (isYearDivisibleByFour && (!century || isYearDivisibleByFourHundred)) {
+       return true;
+   } else {
+       return false;
+   }
 };
 
 // Do not edit below this line
